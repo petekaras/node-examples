dbf70274acf58201cfd1e6e6f4436fe694988c8d
peter <peter@peterkaras.com>
2015-11-11 22:19:15 +0000

    updates

--- read-file/read.js
+++ read-file/read.js
@@ -1,7 +1,15 @@
  var fs = require("fs");
-// Get content from file
+
  var contents = fs.readFileSync("data.json");
-// Define to JSON type
  var jsonContent = JSON.parse(contents);
-// Get Value from JSON
+
+ console.log("Line 1:", jsonContent.default.address.line1);
+
+ jsonContent.default.address.line1 = 'I am a tree';
+
+ console.log("Line 1:", jsonContent.default.address.line1);
+//read again to show how we overwrite existing variable (unlike require)
+ var contents = fs.readFileSync("data.json");
+ var jsonContent = JSON.parse(contents);
+
  console.log("Line 1:", jsonContent.default.address.line1);
